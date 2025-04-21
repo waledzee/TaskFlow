da3f086cdc0ff38a9d3a951eace3b2f0af353aaa
waledzee <waledzaki201@gmail.com>
2025-04-21 12:36:34 +0200

    TaskNotFoundException class is created

--- src/main/java/com/waled/TaskFlow/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/waled/TaskFlow/Exception/GlobalExceptionHandler.java
@@ -1,4 +1,27 @@
 package com.waled.TaskFlow.Exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRunTimeException (RuntimeException ex)
+    {
+        return new ResponseEntity<>("Error: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException  (IllegalArgumentException ex)
+    {
+        return new ResponseEntity<>("Error: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<String> handleTaskNotFoundException(TaskNotFoundException ex) {
+        return new ResponseEntity<>("Not Found: " + ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
 }
---
+++ src/main/java/com/waled/TaskFlow/Exception/TaskNotFoundException.java
@@ -0,0 +1,4 @@
+package com.waled.TaskFlow.Exception;
+
+public class TaskNotFoundException {
+}
